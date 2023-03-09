cd96aa707ced7e39411d7ca6cf55fe6ad2f5a5da
Alexander Dreith <alexander@dreith.com>
2023-03-08 20:00:08 -0700

    add og

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     <>
       <Head>
         <title>Denver Tech Talk</title>
-        <meta name="description" content="Denver Tech Talk" />
+        <meta name="description" content="Community for Colorado coders and tech adjacent professionals or those with an interest." />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
@@ -15,6 +15,7 @@ export default function Home() {
         <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
         <meta name="msapplication-TileColor" content="#da532c" />
         <meta name="theme-color" content="#ffffff" />
+        <meta property="og:image" content="https://denvertechtalk.com/preview.png" />
       </Head>
       <main className={styles.main}>
         <h1 className={styles.title}>Denver Tech Talk<sup>*</sup></h1>
